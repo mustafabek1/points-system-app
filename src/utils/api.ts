6a095a5e38ae2859df08c989b4bebb43004b15e1
Mustafa Bek <mustafabek@Mustafas-MBP.home>
2025-03-17 13:34:40 +0300

    added constante file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { PointsResponse, UsePointsRequest} from '../types';
+import { API_BASE_URL } from './constants/constants';
+
 
 export const fetchPoints = async (userId: string): Promise<PointsResponse> => {
-    const response = await axios.get(`http://localhost:3001/api/points/${userId}`);
+    const response = await axios.get(`${API_BASE_URL}/points/${userId}`);
     return response.data;
 };
 
 export const usePoints = async (userId: string, pointsToUse: number): Promise<UsePointsRequest> => {
-    const response = await axios.post(`http://localhost:3001/api/points/${userId}/use`, {
+    const response = await axios.post(`${API_BASE_URL}/points/${userId}/use`, {
         pointsToUse,
     });
     return response.data;
